4fb8409cf7f77e4bffdd431851aa439e40c393fe
lenconda <i@lenconda.top>
2019-08-15 13:02:59 +0800

    :bug: Fatal: not fount

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
     ),
     filename: 'static/js/' + (config.isDev ? '[name]-routes.js' : '[name]-routes.[contenthash].js'),
     chunkFilename: 'static/js/' + (config.isDev ? '[name].chunk.js' : '[name].[contenthash].chunk.js'),
-    publicPath: config.isDev ? '/' : '//gassess-cdn.lenconda.top'
+    publicPath: config.isDev ? '/' : '//gassess-cdn.lenconda.top/'
   },
   optimization: {
     splitChunks: {
